cb2613ab4b3a410559804a4d2fa58709bf58d846
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-09-19 10:26:36 -0300

    finish!

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import { StatusBar } from "react-native";
 import {
   useFonts,
@@ -7,12 +8,50 @@ import {
   Inter_900Black,
 } from "@expo-google-fonts/inter";
 
+import { Subscription } from "expo-modules-core";
+
 import { Routes } from "./src/routes";
 import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
 
+import "./src/services/notificationsConfig";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+import * as Notifications from "expo-notifications";
+
 export default function App() {
-  // Desta forma ele ira carregar essas fontes para a nossa aplicação
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
@@ -25,7 +64,7 @@ export default function App() {
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
-        translucent={true}
+        translucent
       />
 
       {fontsLoaded ? <Routes /> : <Loading />}
